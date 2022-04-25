e873c67fb7266f96f7c8acb89a2a09a7d42cf033
Marcela Oliveira <mmarcela.oa@gmail.com>
2022-04-25 19:20:24 -0300

    iniciando frontend do blog pessoal com React

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './Home.css';
+
+function Home(){
+    return(
+        <>
+            <h1 className="titulo">Home</h1>
+            <img src="https://i.imgur.com/Ll3Fof3.jpg" alt="gatinho com patinhos de borracha na banheira" className="imagem"/>
+        </>
+    );
+}
+
+export default Home;
