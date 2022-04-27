285f5045aaf1aca8c6feff742f9bf6b972d0c04b
Marcela Oliveira <mmarcela.oa@gmail.com>
2022-04-27 12:07:05 -0300

    criando menu, home e rodapé da aplicação

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import Home from './paginas/home/Home';
 import './App.css';
+import { Grid } from '@material-ui/core';
+import Navbar from './components/statics/navbar/Navbar';
+import Footer from './components/statics/footer/Footer';
 
 function App() {
   return (
+    <>
+    <Navbar />
     <Home />
+    <Footer />
+    </>
   );
 }
 
