fe55f683116be6120ec3fed90220ceec8ddace00
Marcela Oliveira <mmarcela.oa@gmail.com>
2022-04-28 18:34:49 -0300

    criando rotas e organizando css

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,23 @@ import './App.css';
 import { Grid } from '@material-ui/core';
 import Navbar from './components/statics/navbar/Navbar';
 import Footer from './components/statics/footer/Footer';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './paginas/login/Login';
 
 function App() {
   return (
-    <>
-    <Navbar />
-    <Home />
-    <Footer />
-    </>
+    <Router>
+      <Navbar />
+      <div style={{ minHeight: '100vh' }}>
+        <Routes> // Antigo Switch
+          <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+          {/* <Route path="/cadastro" element={<CadastroUsuario />} />  */}
+        </Routes>
+      </div>
+      <Footer />
+    </Router>
   );
 }
 
